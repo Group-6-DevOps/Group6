b7c8529bc1192c3bd4877bac7b5070f72db482a0
40478635 <40478635@napier.ac.uk>
2020-11-13 15:38:31 +0630

    App.java with 4 Query

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,290 @@
 package com.napier.sem;
+//import sql Package
+import java.sql.*;
+import java.util.ArrayList;
 
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        //Display The list of Country by population from largest to smallest
+        System.out.println("\n");
+        System.out.println("The list of country by population from largest to smallest");
+        ArrayList<Country> country = a.CountryLS();
+        a.printCountryLS(country);
+
+        //Display the countries in Asia by population from largest to smallest
+        System.out.println("\n");
+        System.out.println("Country list in Asia by population from largest to smallest");
+        ArrayList<Country> continent = a.continent_countryLS();
+        a.print_countrylist_continentLS(continent);
+
+        //Display the countries in Southeast Asia by population from largest to smallest
+        System.out.println("\n");
+        System.out.println("country list in Southeast Asia by population from largest to smallest");
+        ArrayList<Country> clist_region = a.Region_CountryLS();
+        a.print_countrylist_regionLS(clist_region);
+
+        //Display Top 10 populated countries in the world
+        System.out.println("\n");
+        System.out.println("Top 10 Populated countries in the world");
+        ArrayList<Country> popcnty_world = a.mostpopcountry_worldLS();
+        a.print_countrylist_regionLS(popcnty_world);
+
+        // Disconnect from database
+        a.disconnect();
     }
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 100;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(3000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+    //Query 1
+    public ArrayList<Country> CountryLS()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String ctlist = "SELECT Code, Name, Continent, Region, Population, Capital FROM country ORDER BY Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(ctlist);
+            // Extract employee information
+            ArrayList<Country> Country = new ArrayList<Country>();
+            while (rset.next())
+            {
+                Country cntry = new Country();
+                cntry.setCode(rset.getString("Code"));
+                cntry.setContinent(rset.getString("Continent"));
+                cntry.setName(rset.getString("Name"));
+                cntry.setRegion(rset.getString("Region"));
+                cntry.setPopulation(rset.getInt("Population"));
+                cntry.setCapital(rset.getInt("Capital"));
+                Country.add(cntry);
+            }
+            return Country;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Fail to print Country list");
+            return null;
+        }
+    }
+    public void printCountryLS(ArrayList<Country> Country)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-30s %-45s %-55s %-60s %-65s", "Population", "Code", "Country Name", "continent", "region", "capital" ));
+        // Loop over all employees in the list
+        for (Country emp : Country) {
+            String emp_string =
+                    String.format("%-10s %-30s %-45s %-55s %-60s %-65s",
+                            emp.getPopulation(), emp.getCode(), emp.getName(), emp.getContinent(), emp.getRegion(), emp.getCapital());
+            System.out.println(emp_string);
+        }
+    }
+    //End
+
+
+    //Query 2
+    public ArrayList<Country> continent_countryLS()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String conlist = "SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Continent = 'Asia' ORDER BY Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(conlist);
+            // Extract employee information
+            ArrayList<Country> cty = new ArrayList<Country>();
+            while (rset.next())
+            {
+                Country cont = new Country();
+                cont.setCode(rset.getString("Code"));
+                cont.setContinent(rset.getString("Continent"));
+                cont.setName(rset.getString("Name"));
+                cont.setRegion(rset.getString("Region"));
+                cont.setPopulation(rset.getInt("Population"));
+                cont.setCapital(rset.getInt("Capital"));
+                cty.add(cont);
+            }
+            return cty;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Fail to print Country list");
+            return null;
+        }
+    }
+    public void print_countrylist_continentLS(ArrayList<Country> Country)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-30s %-45s %-55s %-60s %-65s", "Population", "Code", "Country Name", "continent", "region", "capital" ));
+        // Loop over all employees in the list
+        for (Country emp : Country) {
+            String emp_string =
+                    String.format("%-10s %-30s %-45s %-55s %-60s %-65s",
+                            emp.getPopulation(), emp.getCode(), emp.getName(), emp.getContinent(), emp.getRegion(), emp.getCapital());
+            System.out.println(emp_string);
+        }
+    }
+    //End
+
+
+    //Query 3
+    public ArrayList<Country> Region_CountryLS()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String regionlist = "SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Region = 'Southeast Asia' ORDER BY Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(regionlist);
+            // Extract employee information
+            ArrayList<Country> Country = new ArrayList<Country>();
+            while (rset.next())
+            {
+                Country cntry = new Country();
+                cntry.setCode(rset.getString("Code"));
+                cntry.setContinent(rset.getString("Continent"));
+                cntry.setName(rset.getString("Name"));
+                cntry.setRegion(rset.getString("Region"));
+                cntry.setPopulation(rset.getInt("Population"));
+                cntry.setCapital(rset.getInt("Capital"));
+                Country.add(cntry);
+            }
+            return Country;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Fail to print Country list");
+            return null;
+        }
+    }
+    public void print_countrylist_regionLS(ArrayList<Country> Country)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-30s %-45s %-55s %-60s %-65s", "Population", "Code", "Country Name", "continent", "region", "capital" ));
+        // Loop over all employees in the list
+        for (Country emp : Country) {
+            String emp_string =
+                    String.format("%-10s %-30s %-45s %-55s %-60s %-65s",
+                            emp.getPopulation(), emp.getCode(), emp.getName(), emp.getContinent(), emp.getRegion(), emp.getCapital());
+            System.out.println(emp_string);
+        }
+    }
+    //End
+
+
+    //Query 4
+    public ArrayList<Country> mostpopcountry_worldLS()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String regionlist = "SELECT Code, Name, Continent, Region, Population, Capital FROM country ORDER by Population DESC limit 10";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(regionlist);
+            // Extract employee information
+            ArrayList<Country> Country = new ArrayList<Country>();
+            while (rset.next())
+            {
+                Country cntry = new Country();
+                cntry.setCode(rset.getString("Code"));
+                cntry.setContinent(rset.getString("Continent"));
+                cntry.setName(rset.getString("Name"));
+                cntry.setRegion(rset.getString("Region"));
+                cntry.setPopulation(rset.getInt("Population"));
+                cntry.setCapital(rset.getInt("Capital"));
+                Country.add(cntry);
+            }
+            return Country;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Fail to print Country list");
+            return null;
+        }
+    }
+    public void print_popcnty_worldLS(ArrayList<Country> Country1)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-30s %-45s %-55s %-60s %-65s", "Population", "Code", "Country Name", "continent", "region", "capital" ));
+        // Loop over all employees in the list
+        for (Country emp : Country1) {
+            String emp_string =
+                    String.format("%-10s %-30s %-45s %-55s %-60s %-65s",
+                            emp.getPopulation(), emp.getCode(), emp.getName(), emp.getContinent(), emp.getRegion(), emp.getCapital());
+            System.out.println(emp_string);
+        }
+    }
+    //End
+
 }
---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,63 @@
+package com.napier.sem;
+
+public class City {
+    //Create variable for Country_name
+    private String name;
+    //getter for name
+    public String getName() {
+        return name;
+    }
+    // Setter for name
+    public void setName(String newcode) {
+        this.name = newcode;
+    }
+    //end
+
+    //Create variable for Country
+    private String country;
+    //getter for code
+    public String getCountry() {
+        return country;
+    }
+    // Setter for code
+    public void setCountry(String newcode) {
+        this.country = newcode;
+    }
+    //end
+
+    //Create variable for District
+    private String district;
+    //getter for district
+    public String getDistrict() {
+        return district;
+    }
+    // Setter for district
+    public void setDistrict(String newcode) {
+        this.district = newcode;
+    }
+    //end
+
+    //Create variable for population
+    private int population;
+    //getter for population;
+    public int getPopulation() {
+        return population;
+    }
+    // Setter for code
+    public void setPopulation(int newcode) {
+        this.population = newcode;
+    }
+    //end
+
+    //Create variable for continent
+    private String continent;
+    //getter for population;
+    public String getContinent() {
+        return continent;
+    }
+    // Setter for code
+    public void setContinent(String newcode) {
+        this.continent = newcode;
+    }
+    //end
+}
