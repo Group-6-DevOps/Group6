ece3cd9e80a735cb54281d3fdca3b6be2a3fd292
ZarniSoeTun <40478621>
2020-11-20 14:00:28 +0630

    Adding Mock Deploy Stage

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -11,7 +11,14 @@ public class App
         App a = new App();
 
         // Connect to database
-        a.connect("localhost:33060");
+        if (args.length < 1)
+        {
+            a.connect("localhost:3306");
+        }
+        else
+        {
+            a.connect(args[0]);
+        }
 
         //Display The list of Country by population from largest to smallest
         System.out.println("\n");
